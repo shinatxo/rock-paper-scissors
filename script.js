98f572076d11e57a3de1cc2b9be09995ff66b29f
Shina <shinaoguntoye@hotmail.co.uk>
2025-01-22 08:35:58 +0000

    Implement rock-paper-scissors game logic with user and computer choices

---
+++ script.js
@@ -0,0 +1,60 @@
+const getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
+      return userInput;
+    } else {
+      console.log('Error: invalid choice. Please choose rock, paper or scissors.');
+    }
+  };
+  
+  const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+    }
+  };
+  
+  const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === 'bomb') {
+      return 'You used the secret cheat code! You win!';
+    }
+    if (userChoice === computerChoice) {
+      return 'The game is a tie!';
+    } 
+    if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    if (userChoice === 'paper') {
+      if (computerChoice === 'scissors') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    if (userChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+  };
+  
+  const playGame = () => {
+    const userChoice = getUserChoice(prompt('Please choose rock, paper or scissors.'));
+    const computerChoice = getComputerChoice();
+    console.log(`You chose: ${userChoice}`);
+    console.log(`The computer chose: ${computerChoice}`);
+    console.log(determineWinner(userChoice, computerChoice));
+  };
+  
+  playGame();
